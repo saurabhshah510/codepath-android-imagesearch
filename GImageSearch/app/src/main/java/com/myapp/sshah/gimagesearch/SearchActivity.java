dc78feaecd7248b1f9ebafc772e20d1e288d318b
sshah1 <sshah0@walmartlabs.com>
2015-10-17 15:05:56 -0700

    Setting up the views in the activity java file and adding the picasso and async http library for network access and image loading

--- GImageSearch/app/src/main/java/com/myapp/sshah/gimagesearch/SearchActivity.java
+++ GImageSearch/app/src/main/java/com/myapp/sshah/gimagesearch/SearchActivity.java
@@ -2,12 +2,29 @@ package com.myapp.sshah.gimagesearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
+    private EditText etSearchQuery;
+    private GridView gvSearchResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        setupViews();
+    }
+
+    private void setupViews(){
+        etSearchQuery = (EditText)findViewById(R.id.etQuery);
+        gvSearchResults = (GridView)findViewById(R.id.gvSearchResults);
+    }
+
+    public void onClickSearch(View view) {
+        String query = etSearchQuery.getText().toString();
+        Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
     }
 }
