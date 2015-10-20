cb707c7d9d792723ea27061b0354aa61a5929970
sshah1 <sshah0@walmartlabs.com>
2015-10-19 19:02:11 -0700

    Adding the image icon in the search activity and adding menu activity with some radio buttons

---
+++ GImageSearch/app/src/main/java/com/myapp/sshah/gimagesearch/activities/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.myapp.sshah.gimagesearch.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.myapp.sshah.gimagesearch.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
